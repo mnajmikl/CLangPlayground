2ba7af1a20e372e3189f82326d3ecfa477e1c541
Najmi <57437663+mnajmikl@users.noreply.github.com>
2022-09-19 22:42:25 +0800

    Updated code
    Updated code with corrections recommended by Valgrind.

--- Progressbar/showprogress.c
+++ Progressbar/showprogress.c
@@ -5,52 +5,77 @@
 #ifdef _WIN32
 #define FULLBLOCK "\xdb"
 #define HALFBLOCK "\xde"
-#endif // _WIN32
-
-#ifdef _linux_
+#elif __linux__
 #define FULLBLOCK "\u2588"
 #define HALFBLOCK "\u2590"
-#endif // linux
+#endif
 
-char *repeatchar(char *, size_t);
-char *showprogress(size_t, size_t);
+static char *repeatchar(const char*, int);
+void showprogress(int, int);
 
-int main() {
-    char *progresscounter, *progressbar;
+int main()
+{
     printf("\nRepeat characters 100 times\n");
-    for(size_t i = 0; i <= 25; i++) {
-        progresscounter = repeatchar(FULLBLOCK, i);
-        printf("\r|%s|\r", progresscounter);
+
+    for(int i = 0; i <= 100; i++)
+    {
+        char* progresscounter = repeatchar(FULLBLOCK, i);
+        printf("\r|%s|%3d\r", progresscounter, i);
+        free(progresscounter);
     }
-    free(progresscounter);
-    printf("\nShow Progress 0 - 200000\n");
-    for(size_t i = 0; i <= 200000; i++) {
-        progressbar = showprogress(i, 200000);
-        printf("\r%s\r", progressbar);
-    }
+
     printf("\r\n");
+
+    for(int i = 0; i <= 200000; i++)
+        showprogress(i, 200000);
+
+    printf("\n");
+
     fflush(stdout);
-    free(progressbar);
+
     return 0;
 }
 
-char *repeatchar(char *str, size_t count) {
-    if (count == 0) return str;
-    char *repeat = calloc(strlen(str) * count + count, sizeof(size_t));
-    if (repeat == NULL) return "";
+static char *repeatchar(const char *str, int count) {
+    char* cpy = calloc(strlen(str) + 1, sizeof(char));
+    if (cpy)
+        strcpy(cpy, str);
+    else
+        return "";
+
+    if (count == 0)
+        return cpy;
+
+    char* repeat = calloc((strlen(str) * count) + 1, sizeof(char));
+
+    if (repeat == NULL)
+        return "";
+
     while (count--)
-        strcat(repeat, str);
+        strcat(repeat, cpy);
+
+    if (cpy != NULL)
+        free(cpy);
+
     return repeat;
 }
 
-char *showprogress(size_t progress, size_t total)
+void showprogress(int progress, int total)
 {
-    char *current_cursor, *next_cursor, *result;
-    float percent = 100 * ((float)progress / (float)total);
-    current_cursor = repeatchar(FULLBLOCK, ((size_t)percent / 4));
-    next_cursor = repeatchar(HALFBLOCK, 25 - ((size_t)percent / 4));
-    result = calloc(strlen(current_cursor) + strlen(next_cursor) + (size_t)percent + 1, sizeof(size_t));
-    if ((int)percent == 100) next_cursor = "";
-    sprintf(result, "|%s%s|%.2f%%", current_cursor, next_cursor, percent);
-    return result;
+    float percentage = 100.00 * ((float)progress / total);
+
+    char* current_cursor = repeatchar(FULLBLOCK, (int)percentage);
+
+    int next_cur = (int)percentage == 100 ? 100 : (100 - (int)percentage);
+
+    char* ch_next_cur = (int)percentage == 100 ? "" : HALFBLOCK;
+
+    char* next_cursor = repeatchar(ch_next_cur, next_cur);
+
+    printf("\r|%s%s|%.2f%%\r", current_cursor, next_cursor, percentage);
+
+    if (current_cursor != NULL)
+        free(current_cursor);
+    if (next_cursor != NULL)
+        free(next_cursor);
 }
